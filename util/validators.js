fdee41c870763bd3727b667da2f47810ee53e497
akaash-trivedi <akaash.t1@gmail.com>
2020-10-26 19:19:53 +0530

    Login and login validation

--- util/validators.js
+++ util/validators.js
@@ -22,3 +22,17 @@ module.exports.validateRegisterInput = (username, email, password, confirmPasswo
 		valid: Object.keys(errors).length < 1
 	};
 };
+
+module.exports.validateLoginInput = (username, password) => {
+	const errors = {};
+	if (username.trim() === '') {
+		errors.username = 'Username must not be empty';
+	}
+	if (password.trim() === '') {
+		errors.password = 'Password must not be empty';
+	}
+	return {
+		errors,
+		valid: Object.keys(errors).length < 1
+	};
+};
